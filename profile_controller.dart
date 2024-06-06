9acc6cfad8d1afb0914327e92e10648ea21f3f4b
mani42402 <130709906+mani42402@users.noreply.github.com>
2024-06-07 00:05:30 +0500

    Add files via upload

---
+++ profile_controller.dart
@@ -0,0 +1,132 @@
+import 'package:get/get.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:tiktok_practice/constraints.dart';
+
+class ProfileController extends GetxController {
+  final Rx<Map<String, dynamic>> _user = Rx<Map<String, dynamic>>({});
+  Map<String, dynamic> get user => _user.value;
+
+  Rx<String> _uid = "".obs;
+
+  updateUserId(String uid) {
+    _uid.value = uid;
+    getUserData();
+  }
+
+  getUserData() async {
+    List<String> thumbnails = [];
+    List<String> videos = [];
+    var myVideos = await firestore
+        .collection('videos')
+        .where('uid', isEqualTo: _uid.value)
+        .get();
+
+    for (var video in myVideos.docs) {
+      thumbnails.add(video['thumbnail']);
+      videos.add(video['videoUrl']);
+    }
+
+    DocumentSnapshot userDoc =
+    await firestore.collection('users').doc(_uid.value).get();
+    final userData = userDoc.data()! as dynamic;
+    String name = userData['name'];
+    String profilePhoto = userData['profilePhoto'];
+    int likes = 0;
+    int followers = 0;
+    int following = 0;
+    bool isFollowing = false;
+
+    for (var item in myVideos.docs) {
+      likes += (item.data()['likes'] as List).length;
+    }
+    var followerDoc = await firestore
+        .collection('users')
+        .doc(_uid.value)
+        .collection('followers')
+        .get();
+    var followingDoc = await firestore
+        .collection('users')
+        .doc(_uid.value)
+        .collection('following')
+        .get();
+    followers = followerDoc.docs.length;
+    following = followingDoc.docs.length;
+
+    firestore
+        .collection('users')
+        .doc(_uid.value)
+        .collection('followers')
+        .doc(authController.user.uid)
+        .get()
+        .then((value) {
+      if (value.exists) {
+        isFollowing = true;
+      } else {
+        isFollowing = false;
+      }
+    });
+
+    _user.value = {
+      'followers': followers.toString(),
+      'following': following.toString(),
+      'isFollowing': isFollowing,
+      'likes': likes.toString(),
+      'profilePhoto': profilePhoto,
+      'name': name,
+      'thumbnails': thumbnails,
+      'videos': videos,
+    };
+    update();
+  }
+
+  List<String> get userVideos => _user.value['videos'];
+
+  List<String> get userThumbnails => _user.value['thumbnails'];
+
+  followUser() async {
+    var doc = await firestore
+        .collection('users')
+        .doc(_uid.value)
+        .collection('followers')
+        .doc(authController.user.uid)
+        .get();
+
+    if (!doc.exists) {
+      await firestore
+          .collection('users')
+          .doc(_uid.value)
+          .collection('followers')
+          .doc(authController.user.uid)
+          .set({});
+      await firestore
+          .collection('users')
+          .doc(authController.user.uid)
+          .collection('following')
+          .doc(_uid.value)
+          .set({});
+      _user.value.update(
+        'followers',
+            (value) => (int.parse(value) + 1).toString(),
+      );
+    } else {
+      await firestore
+          .collection('users')
+          .doc(_uid.value)
+          .collection('followers')
+          .doc(authController.user.uid)
+          .delete();
+      await firestore
+          .collection('users')
+          .doc(authController.user.uid)
+          .collection('following')
+          .doc(_uid.value)
+          .delete();
+      _user.value.update(
+        'followers',
+            (value) => (int.parse(value) - 1).toString(),
+      );
+    }
+    _user.value.update('isFollowing', (value) => !value);
+    update();
+  }
+}
